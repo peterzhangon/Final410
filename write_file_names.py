17f6b193b4d286ee8c198fd3a15c323f26cc5077
peter zhang <peterzhangon@gmail.com>
2021-12-02 20:41:22 -0500

    Update write_file_names.py
    to add pre-process script + web + extractor

--- write_file_names.py
+++ write_file_names.py
@@ -1,24 +1,26 @@
 import os,codecs,json
 
-def main(dir_,out_file1,out_file2,dept_path,uni_path,names_path,url_path,loc_path,email_path,filter_file1,filter_file2):
+def main(base_data_dir,output_dataset_full_corpus,output_meta_data,input_dept_path,uni_path,input_names_path,input_url_path,input_loc_path,input_email_path,input_phone_num,output_unis_filter_file,output_locs_filter_file):
 	with open(uni_path,'r') as f:
 		unis = f.readlines()
 
-	with open(dept_path,'r') as f:
+	with open(input_dept_path,'r') as f:
 		depts = f.readlines()
 
-	with open(names_path,'r') as f:
+	with open(input_names_path,'r') as f:
 		names = f.readlines()
 
-	with open(url_path,'r') as f:
+	with open(input_url_path,'r') as f:
 		urls = f.readlines()
 
-	with codecs.open(loc_path,'r',encoding='utf-8',errors='ignore') as f:
+	with codecs.open(input_loc_path,'r',encoding='utf-8',errors='ignore') as f:
 		locs = f.readlines()
 
-	with codecs.open(email_path,'r',encoding='utf-8',errors='ignore') as f:
+	with codecs.open(input_email_path,'r',encoding='utf-8',errors='ignore') as f:
 		emails = f.readlines()
 
+	with codecs.open(input_phone_num,'r',encoding='utf-8',errors='ignore') as f:
+		phones = f.readlines()
 
 	max_len = 15
 	max_parts = 3
@@ -34,27 +36,31 @@ def main(dir_,out_file1,out_file2,dept_path,uni_path,names_path,url_path,loc_pat
 				corrected_name += ' '+part.title()
 		corrected_names.append(corrected_name)
 
+	# The real file numbers
+	num_bios = len(os.listdir(base_data_dir))-5
 
-	num_bios = len(os.listdir(dir_))-5
+	print(len(phones),len(emails),len(corrected_names),len(locs),len(depts),len(unis),num_bios)
 
-	print(emails[-2:],len(emails),len(corrected_names),len(locs),len(depts),len(unis),num_bios)
-
-	with open(out_file1,'w') as f1:
-		with codecs.open(out_file2,'w',encoding='utf-8',errors='ignore') as f2:
+	with open(output_dataset_full_corpus,'w') as f1:
+		with codecs.open(output_meta_data,'w',encoding='utf-8',errors='ignore') as f2:
 			for i in range(num_bios)[:-1]:
 				f1.write('[None] '+str(i)+'.txt')
 				f1.write('\n')
 				if emails[i]=='\n':
 					emails[i]='None'
+				if phones[i]=='\n':
+					phones[i]='None'
 				f2.write(str(i)+'.txt'+'\t'
-					+unis[i].strip()+'\t'+depts[i].strip()+'\t'+corrected_names[i]+'\t'+urls[i].strip()+'\t'+locs[i].strip()+'\t'+emails[i].strip())
+					+unis[i].strip()+'\t'+depts[i].strip()+'\t'+corrected_names[i]+'\t'+urls[i].strip()+'\t'+locs[i].strip()+'\t'+emails[i].strip()+'\t'+phones[i].strip())
 				f2.write('\n')
 
 			f1.write('[None] '+str(num_bios-1)+'.txt')
 			if emails[num_bios-1]=='\n':
-					emails[num_bios-1]='None'
+				emails[num_bios-1]='None'
+			if phones[num_bios-1]=='\n':
+				phones[num_bios-1]='None'
 			f2.write(str(num_bios-1)+'.txt'+'\t'
-				+unis[num_bios-1].strip()+'\t'+depts[num_bios-1].strip()+'\t'+corrected_names[num_bios-1]+'\t'+urls[num_bios-1].strip()+'\t'+locs[num_bios-1].strip()+'\t'+emails[num_bios-1].strip())
+				+unis[num_bios-1].strip()+'\t'+depts[num_bios-1].strip()+'\t'+corrected_names[num_bios-1]+'\t'+urls[num_bios-1].strip()+'\t'+locs[num_bios-1].strip()+'\t'+emails[num_bios-1].strip()+'\t'+phones[num_bios-1].strip())
 
 	unis_dict = {"unis":sorted([uni.strip() for uni in list(set(unis))])}
 	all_countries = set()
@@ -71,14 +77,33 @@ def main(dir_,out_file1,out_file2,dept_path,uni_path,names_path,url_path,loc_pat
 
 	locs_dict = {"locs":all_locs}
 
-	json.dump(unis_dict,open(filter_file1,'w'))
-	json.dump(locs_dict,open(filter_file2,'w'))
-
-	
+	json.dump(unis_dict,open(output_unis_filter_file,'w'))
+	json.dump(locs_dict,open(output_locs_filter_file,'w'))
 
 if __name__ == '__main__':
-	main('../data/compiled_bios','../compiled_bios/dataset-full-corpus.txt','../data/compiled_bios/metadata.dat','../data/depts','../data/unis','../data/names.txt','../data/urls','../data/location','../data/emails','../data/filter_data/unis.json','../data/filter_data/locs.json')
+	base_data_dir = './data/compiled_bios'
+	print("base_data_dir : {0}".format(os.path.abspath(base_data_dir)))
+	output_dataset_full_corpus = './data/compiled_bios/dataset-full-corpus.txt'
+	print("output_dataset_full_corpus : {0}".format(os.path.abspath(output_dataset_full_corpus)))
+	output_meta_data = './data/compiled_bios/metadata.dat'
+	print("output_meta_data : {0}".format(os.path.abspath(output_meta_data)))
+	input_dept_path = './data/depts'
+	print("input_dept_path : {0}".format(os.path.abspath(input_dept_path)))
+	uni_path = './data/unis'
+	print("uni_path : {0}".format(os.path.abspath(uni_path)))
+	input_names_path = './data/names.txt'
+	print("input_names_path : {0}".format(os.path.abspath(input_names_path)))
+	input_url_path = './data/urls'
+	print("input_url_path : {0}".format(os.path.abspath(input_url_path)))
+	input_loc_path = './data/location'
+	print("input_loc_path : {0}".format(os.path.abspath(input_loc_path)))
+	input_email_path = './data/emails'
+	print("input_email_path : {0}".format(os.path.abspath(input_email_path)))
+	input_phone_num = './data/phones'
+	print("input_phone_num : {0}".format(os.path.abspath(input_phone_num)))
+	output_unis_filter_file = './data/filter_data/unis.json'
+	print("output_unis_filter_file : {0}".format(os.path.abspath(output_unis_filter_file)))
+	output_locs_filter_file = './data/filter_data/locs.json'
+	print("output_locs_filter_file : {0}".format(os.path.abspath(output_locs_filter_file)))
 
-
-
-
+	main(base_data_dir, output_dataset_full_corpus, output_meta_data, input_dept_path, uni_path, input_names_path, input_url_path, input_loc_path, input_email_path, input_phone_num, output_unis_filter_file, output_locs_filter_file)
